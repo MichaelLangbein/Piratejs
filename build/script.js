0712797281f7d556cfac1e5ed8851a2f2fd219c5
Michael Langbein <michael.langbein@lfu.bayern.de>
2018-04-21 18:34:28 +0200

    p5 now written in specified div

--- build/script.js
+++ build/script.js
@@ -80909,4 +80909,15 @@ var p5 = require('p5');
 var socket = require('socket.io-client')('10.112.70.159:3000');
 
 
+var sketch = function(p) {
+	p.setup = function () {
+		p.createCanvas(400, 400);
+		p.background(0);
+	};
+
+	p.draw = function () {};
+};
+
+new p5(sketch, 'processingCanvas');
+
 },{"p5":29,"socket.io-client":33}]},{},[44]);
--- src/client/client.js
+++ src/client/client.js
@@ -1,3 +1,14 @@
 var p5 = require('p5');
 var socket = require('socket.io-client')('10.112.70.159:3000');
 
+
+var sketch = function(p) {
+	p.setup = function () {
+		p.createCanvas(400, 400);
+		p.background(0);
+	};
+
+	p.draw = function () {};
+};
+
+new p5(sketch, 'processingCanvas');
