31a3ecd94a9167a4b5dae0a4a34c01531cc3cb2c
rinso86 <michael_langbein@hotmail.de>
2018-04-26 22:38:22 +0200

    fdsfds

--- src/ai/AiShip.js
+++ src/ai/AiShip.js
@@ -3,46 +3,56 @@ var NN = require('./nn.js');
 
 
 var Eyes = function (engine) {
+	
 	this.engine = engine;
 
-	this.findNearestEnemy = function(){
-		
+	this.findNearestEnemy = function () {
+		var agents = this.engine.agents;
+		for(var i = 0; i < agents.length; i++) {
+			var agent = agents[i];
+			if(agent.type == "Ship" && agent.id != this.id) {
+				return agent;
+			}
+		}
+		return agents[0];
 	};
 };
 
 var AiShip = function (engine) {
+
+	this.id = Math.random() * 1000;
 
 	this.eyes = new Eyes(engine);
 
 	this.brain = new NN([8, 10, 10, 4]);
 
 	this.update = function () {
-		this.think();
+		var result = this.think();
+		this.takeAction(result);
 		this.move();
 		this.updateCooldown();
 	};
 
 	this.think = function () {
-		var nearestEnemy = this.eyes.findNearestEnemy();
-		var result = this.brain.evaluate([this.posX, this.posY, this.angle, this.speed, ne.posX, ne.posY, ne.angle, ne.speed]);
-		this.takeAction(result);
+		var ne = this.eyes.findNearestEnemy();
+		var results = this.brain.evaluate([this.posX, this.posY, this.angle, this.speed, ne.posX, ne.posY, ne.angle, ne.speed]);
+		return results.indexOf( Math.max(...results) );
 	};
 
 	this.takeAction = function (result) {
-		var max = this.indexOfMax(result);
-		switch(max){
-			case 0:
-				this.angle +=1;
+		switch (result) {
+			case 0: 
+				this.angle += 1;
 				break;
 			case 1:
-				this.angle -=1;
+				this.angle -= 1;
 				break;
 			case 2: 
 				this.shoot("starboard");
 				break;
-			case 3:
+			case 3: 
 				this.shoot("larboard");
-				break;
+				break;	
 		}
 	};
 
